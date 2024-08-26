3e0598b7f45e586dd5be94c15865a16ffedb22b8
alin4ik_kvlnk <alinkakovalenko98@gmail.com>
2024-08-26 19:04:59 +0200

    Add IRepository

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        //T Get(int id);
+        T Get(Expression<Func<T, bool>> filter);
+        //void Update(T entity);
+        void Remove(T entity);  
+        void Add(T entity); 
+        void RemoveRange(IEnumerable<T> entity);
+
+    }
+}
