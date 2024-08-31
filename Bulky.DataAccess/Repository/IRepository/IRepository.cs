6670d27d56120a3ae977fc7a4aedf7696dfb3d99
alin4ik_kvlnk <alinkakovalenko98@gmail.com>
2024-08-31 21:29:32 +0200

    add new

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -6,9 +6,9 @@ namespace Bulky.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAll(string? includeProperties = null);
         //T Get(int id);
-        T Get(Expression<Func<T, bool>> filter);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
         //void Update(T entity);
         void Remove(T entity);  
         void Add(T entity); 
